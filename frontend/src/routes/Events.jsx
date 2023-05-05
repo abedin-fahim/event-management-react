8355d532bfcad7e55eea91df33c0bc47362b9482
Joynal Abedin <cfcfahim26@gmail.com>
2023-05-05 22:27:23 +0600

    loaders added

--- frontend/src/routes/Events.jsx
+++ frontend/src/routes/Events.jsx
@@ -1,10 +1,11 @@
 import { Fragment } from 'react';
-
 import { useLoaderData } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
+  // We can use the useLoaderData inside of the EventsList component directly.
+  // We can't use it in the higher level route.
   const events = useLoaderData();
 
   return (
