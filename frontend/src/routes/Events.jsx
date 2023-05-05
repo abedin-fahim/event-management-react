4864fb50b5f6deeb18a48883c029756bea19c908
Joynal Abedin <cfcfahim26@gmail.com>
2023-05-05 22:51:50 +0600

    loader in react router

--- frontend/src/routes/Events.jsx
+++ frontend/src/routes/Events.jsx
@@ -6,7 +6,8 @@ import EventsList from '../components/EventsList';
 function EventsPage() {
   // We can use the useLoaderData inside of the EventsList component directly.
   // We can't use it in the higher level route.
-  const events = useLoaderData();
+  const data = useLoaderData();
+  const events = data.events;
 
   return (
     <Fragment>
@@ -20,12 +21,14 @@ export default EventsPage;
 // Common pattern of storing the data is in the file we are using the loader in.
 // We import the function in router file where we declare the loader
 export async function loader() {
+  // We can't react hooks here 
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
     // ...
   } else {
-    const resData = await response.json();
-    return resData.events;
+    // const resData = await response.json();
+    // return resData.events;
+    return response;
   }
 }
--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -1,17 +1,21 @@
-import { Fragment } from "react";
-import { Outlet } from "react-router-dom";
+import { Fragment } from 'react';
+import { Outlet, useNavigation } from 'react-router-dom';
 
-import MainNavigation from '../components/MainNavigation'
+import MainNavigation from '../components/MainNavigation';
 
 const RootLayout = () => {
-    return (
-        <Fragment>
-            <MainNavigation />
-            <main>
-                <Outlet />
-            </main>
-        </Fragment>
-    );
+  // We can use useNavigation hook from react-router to check and provide better feedback to the user about the transition
+  // const navigation = useNavigation();
+
+  return (
+    <Fragment>
+      <MainNavigation />
+      {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+      <main>
+        <Outlet />
+      </main>
+    </Fragment>
+  );
 };
 
 export default RootLayout;
