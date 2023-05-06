bcbbfeb94243ffdf0c5c71dc1a03be7bb53fb904
Joynal Abedin <cfcfahim26@gmail.com>
2023-05-06 20:00:36 +0600

    error handling added within loader

--- frontend/src/routes/Error.jsx
+++ frontend/src/routes/Error.jsx
@@ -1,12 +1,33 @@
-import { Fragment } from "react";
+import { Fragment } from 'react';
+
+import { useRouteError } from 'react-router-dom';
+
+import PageContent from '../components/PageContent';
+import MainNavigation from '../components/MainNavigation';
 
 const ErrorPage = () => {
-    return (
-        <Fragment> 
-            <h1>404 not found!</h1>
-            <p>The link you were looking for is not found.</p>
-        </Fragment>
-    );
-}
+  const error = useRouteError();
+
+  let title = 'An error occurred!';
+  let message = 'Something went wrong';
+
+  if (error.status === 500) {
+    let data = JSON.parse(error.data);
+    message = data.message;
+  }
+
+  if (error.status === 404) {
+    title = 'Page not found';
+    message = 'Sorry for the inconvenience!';
+  }
+  return (
+    <Fragment>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </Fragment>
+  );
+};
 
 export default ErrorPage;
--- frontend/src/routes/Events.jsx
+++ frontend/src/routes/Events.jsx
@@ -7,6 +7,12 @@ function EventsPage() {
   // We can use the useLoaderData inside of the EventsList component directly.
   // We can't use it in the higher level route.
   const data = useLoaderData();
+
+  // One way of handling errors
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
+
   const events = data.events;
 
   return (
@@ -21,12 +27,16 @@ export default EventsPage;
 // Common pattern of storing the data is in the file we are using the loader in.
 // We import the function in router file where we declare the loader
 export async function loader() {
-  // We can't react hooks here 
+  // We can't react hooks here
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
-    // ...
+    // return { isError: true, message: 'Could not fetch events' };
+    throw new Response(JSON.stringify({ message: 'Could not fetch events' }), {
+      status: 500,
+    });
   } else {
+    // Bc react-router supports handling responses
     // const resData = await response.json();
     // return resData.events;
     return response;
